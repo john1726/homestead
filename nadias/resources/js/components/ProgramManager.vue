986a7a4f49180c5045d4f9a1826e0a50f15a944a
john1726 <john_1726@yahoo.com>
2023-07-08 15:56:10 -0600

    -Completed "Building Laravel and Vue.js Web Apps: Module 2: MV* Architectures: Managing scrolling and focus"

--- nadias/resources/js/components/ProgramManager.vue
+++ nadias/resources/js/components/ProgramManager.vue
@@ -1,12 +1,14 @@
 <template>
     <form>
+        <a @click="addProgram" class="add">+ Add Program</a>
         <div v-for="(program, index) in programs" :key="program.id">
             <input type="text" v-model="program.title">
             <input type="number" v-model="program.display_order">
             <a @click="removeProgram(index)" class="remove">delete</a>
             <div>
-                <img :src="`/images/${program.image}`" width="100">
-                <input type="text" v-model="program.image">
+                <img v-if="program.image" :src="`/images/${program.image}`" width="100">
+                <label v-else>Image: </label>
+                <input type="text" v-model.lazy="program.image">
             </div>
             <hr> <!-- horizontal rule to separate programs -->
         </div>
@@ -28,6 +30,18 @@
                 if (confirm('Are you sure?')) {
                     this.programs.splice(index, 1);
                 }
+            },
+            addProgram() {
+                this.programs.push({
+                    id: 0,
+                    name: '',
+                    image: '',
+                    display_order: this.programs.length + 1
+                });
+                this.$nextTick(() => {
+                    window.scrollTo(0, document.body.scrollHeight);
+                    this.$refs[''][0].focus();
+                });
             }
         }
     }
